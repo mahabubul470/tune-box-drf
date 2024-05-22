e0b3bcaee42d29e36898e330ae393cb5d56587ba
Mahabubul Hasan <mahabubul470@gmail.com>
2024-05-22 17:23:16 +0600

    Change to sqlite

--- tune_box_drf/settings.py
+++ tune_box_drf/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'tune_box_drf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # DATABASES = {
@@ -98,16 +98,16 @@ WSGI_APPLICATION = 'tune_box_drf.wsgi.application'
 # }
 
 
-# Import dj-database-url at the beginning of the file.
-import dj_database_url
-# Replace the SQLite DATABASES configuration with PostgreSQL:
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-}
+# # Import dj-database-url at the beginning of the file.
+# import dj_database_url
+# # Replace the SQLite DATABASES configuration with PostgreSQL:
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://postgres:postgres@localhost:5432/mysite',
+#         conn_max_age=600
+#     )
+# }
 
 
 
